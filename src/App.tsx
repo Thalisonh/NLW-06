58f3880a174f828232c478e54de325c57653a176
thalisonh <thalison_henrique@hotmail.com>
2021-06-22 22:20:09 -0300

    first day NLW

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+
+import { BrowserRouter , Route } from 'react-router-dom';
+
 import { Button } from "./components/Button";
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
-    <div>
-      <Button/>
-      <Button />
-    </div>
+    <BrowserRouter>
+      <Route path="/" exact component={Home}/>
+      <Route path="/rooms/new" component={NewRoom}/>
+    </BrowserRouter>
     
   );
 }
